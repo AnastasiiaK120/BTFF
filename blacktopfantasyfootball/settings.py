5a58a04591f5cd1ae48589577411612f3ec91f84
anastasiia <anastasiiakudriavtseva4@gmail.com>
2022-08-20 21:31:50 -0400

    Postgres

--- blacktopfantasyfootball/settings.py
+++ blacktopfantasyfootball/settings.py
@@ -85,10 +85,25 @@ WSGI_APPLICATION = 'blacktopfantasyfootball.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'football_new',
+        'USER': 'myuser15',
+        'PASSWORD': 'pwd1345',
+
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
